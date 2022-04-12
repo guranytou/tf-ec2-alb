f33b45405403e0eca8b61eb4da09bab42aaf5bb7
guranytou <guranytou@gmail.com>
2022-04-12 15:19:25 +0900

    リファクタリングとREADME作成

--- aws_instance.tf
+++ aws_instance.tf
@@ -2,7 +2,7 @@
 # EC2 - Web Server
 ################################################################################
 
-resource "aws_instance" "example_1" {
+resource "aws_instance" "example" {
   ami                    = "ami-0992fc94ca0f1415a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.example_http.id]
@@ -11,6 +11,104 @@ resource "aws_instance" "example_1" {
   user_data = file("install_apache.sh")
 
   tags = {
-    "Name" = "example_1"
+    Name = "example"
   }
+}
+
+################################################################################
+# Security group - HTTP allow
+################################################################################
+
+resource "aws_security_group" "example_http" {
+  name   = "example_http"
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_security_group_rule" "sg_ingress" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.example_http.id
+}
+
+resource "aws_security_group_rule" "sg_egress" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.example_http.id
+}
+
+
+
+################################################################################
+# Load Balancer - Web Server
+################################################################################
+
+resource "aws_lb" "example" {
+  name                             = "example"
+  load_balancer_type               = "application"
+  internal                         = false
+  idle_timeout                     = 60
+  enable_cross_zone_load_balancing = false
+  security_groups                  = [aws_security_group.example_http.id]
+  subnets = [
+    aws_subnet.example_public_1a.id,
+    aws_subnet.example_public_1c.id
+  ]
+
+  tags = {
+    Name = "example"
+  }
+}
+
+################################################################################
+# Load Balancer Listener - Web Server
+################################################################################
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
+
+################################################################################
+# Target Group - Web Server
+################################################################################
+
+resource "aws_lb_target_group" "example" {
+  name     = "example"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.example.id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = 80
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.example]
+}
+
+resource "aws_lb_target_group_attachment" "example" {
+  target_group_arn = aws_lb_target_group.example.arn
+  target_id        = aws_instance.example.id
+  port             = 80
 }
--- aws_vpc.tf
+++ aws_vpc.tf
@@ -12,3 +12,131 @@ resource "aws_vpc" "example" {
     "Name" = "example"
   }
 }
+
+################################################################################
+# Subnet - public
+################################################################################
+
+resource "aws_subnet" "example_public_1a" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.100.0.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "example_public_1a"
+  }
+}
+
+resource "aws_subnet" "example_public_1c" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.100.1.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "example_public_1c"
+  }
+}
+
+################################################################################
+# Subnet - private
+################################################################################
+
+resource "aws_subnet" "example_private_1a" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.100.100.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name" = "example_private_1"
+  }
+}
+
+
+################################################################################
+# Internet Gateway
+################################################################################
+
+resource "aws_internet_gateway" "example_1" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    "Name" = "example_1"
+  }
+}
+
+################################################################################
+# NAT Gateway
+################################################################################
+
+resource "aws_nat_gateway" "example_1" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.example_public_1a.id
+  depends_on    = [aws_internet_gateway.example_1]
+
+  tags = {
+    "Name" = "example_1"
+  }
+}
+
+################################################################################
+# Elastic IP - Web Server(NAT Gateway)
+################################################################################
+
+resource "aws_eip" "nat_gw" {
+  vpc = true
+}
+
+
+################################################################################
+# Route Table - Public
+################################################################################
+
+resource "aws_route_table" "example_internet_gw" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    "Name" = "example_route_internet_gw"
+  }
+}
+
+resource "aws_route" "example_internet_gw_default" {
+  route_table_id         = aws_route_table.example_internet_gw.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.example_1.id
+}
+
+resource "aws_route_table_association" "example_internet_gw_1a" {
+  subnet_id      = aws_subnet.example_public_1a.id
+  route_table_id = aws_route_table.example_internet_gw.id
+}
+
+resource "aws_route_table_association" "example_internet_gw_1c" {
+  subnet_id      = aws_subnet.example_public_1c.id
+  route_table_id = aws_route_table.example_internet_gw.id
+}
+
+################################################################################
+# Route Table - Private
+################################################################################
+
+resource "aws_route_table" "example_private" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    "Name" = "example_route_private"
+  }
+}
+
+resource "aws_route" "example_private_default" {
+  route_table_id         = aws_route_table.example_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.example_1.id
+}
+
+resource "aws_route_table_association" "example_private_1" {
+  subnet_id      = aws_subnet.example_private_1a.id
+  route_table_id = aws_route_table.example_private.id
+}
